9bfeef4dfff04b330a91423a9cd2a1ca47d37900
junkafarian <fergus.doyle@largeblue.com>
2009-03-11 19:55:49 +0000

    added some tests

--- mint/repoze/auth.py
+++ mint/repoze/auth.py
@@ -39,15 +39,19 @@ from repoze.bfg.interfaces import IRootFactory
 
 from repoze.zodbconn.finder import dbfactory_from_uri
 
+def default_checker(password, against):
+    return password == against
+
 class ZODBPlugin:
     implements(IAuthenticator)
     dbfactory = staticmethod(dbfactory_from_uri) # for testing override
     
-    def __init__(self, zodb_uri, users_finder, base):
+    def __init__(self, zodb_uri, users_finder, base, checker=default_checker):
         self.zodb_uri = zodb_uri
         self.users_finder = users_finder
         self.base = base
         self.db = None
+        self.checker = checker
     
     def _getdb(self):
         if self.db is None:
@@ -66,7 +70,7 @@ class ZODBPlugin:
         try:
             users = self._getusers(conn)
             user = users.get(identity['login'], None)
-            if user and user.password == identity['password']:
+            if user and self.checker(identity['password'], user.password):
                 return user.id
         finally:
             conn.close()
@@ -78,11 +82,6 @@ def middleware(app, base):
     from repoze.who.plugins.basicauth import BasicAuthPlugin
     from repoze.who.plugins.auth_tkt import AuthTktCookiePlugin
     from repoze.who.plugins.form import FormPlugin
-    #from repoze.who.plugins.htpasswd import HTPasswdPlugin
-    
-    salt = 'aa'
-    def cleartext_check(password, hashed):
-        return password == hashed
     
     def find_users(root, base):
         from mint.repoze.root import ZODBInit
@@ -90,8 +89,7 @@ def middleware(app, base):
         mint_root = init(root)
         return mint_root['users']
     
-    zodb = ZODBPlugin('zeo://localhost:8100', find_users, base)
-    #htpasswd = HTPasswdPlugin('mint.passwd', cleartext_check)
+    zodb = ZODBPlugin('zeo://localhost:8100', find_users, base, checker=default_checker)
     basicauth = BasicAuthPlugin('Mint')
     auth_tkt = AuthTktCookiePlugin('secret', 'auth_tkt')
     # move to RedirectingFormPlugin
